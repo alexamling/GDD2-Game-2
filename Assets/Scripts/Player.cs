c2c1b48be6cf937b3fbb454ef897d6eb18d38444
alexamling <alexamling14@gmail.com>
2020-03-05 17:24:55 -0500

    Auto stash before merge of "PlayerAttack" and "origin/master"

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,9 +4,28 @@ using UnityEngine;
 
 public class Player : Unit
 {
+    public ProjectileData[] projectilePresets;
+
+    private Transform inactiveProjectiles;
+    private Transform activeProjectiles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inactiveProjectiles = new GameObject().transform;
+        activeProjectiles = new GameObject().transform;
+        inactiveProjectiles.parent = transform;
+        activeProjectiles.parent = transform;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void FireProjectile(ProjectileData type)
+    {
+
     }
 }
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public struct ProjectileData
+{
+    public float damage;
+    public float range;
+    public float speed;
+    public float radius;
+    public ParticleSystem effect;
+    public TrailRenderer trail;
+}
+
+public class Projectile : MonoBehaviour
+{
+    private Vector3 direction;
+
+    Projectile(Vector3 dir, ProjectileData data)
+    {
+        direction = dir;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
